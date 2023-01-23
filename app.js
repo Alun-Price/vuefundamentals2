552c890368737c6a88b5a43254661ef58f8c574f
Alun Price <alunprice089@gmail.com>
2023-01-23 15:05:12 +0000

    s2 28 Binding styles

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ let vm = Vue.createApp({
     return {
       isPurple: false,
       selectedColor: "",
+      size: 150,
     };
   },
   computed: {
     circle_classes() {
       return { purple: this.isPurple };
+    },
+    circle_style() {
+      return {
+        width: this.size + "px",
+        height: this.size + "px",
+        lineHeight: this.size + "px",
+      };
     },
   },
 }).mount("#app");
